413b4884ac2ef1bfe1f4e841799c24d173103c02
mrcai <mrcai@asu.edu>
2020-10-08 23:54:57 -0700

    Modified for Readability/Consistency

--- sqlQueries/queryPollutantByState.sql
+++ sqlQueries/queryPollutantByState.sql
@@ -1,23 +1,21 @@
---Date limits provided
---Not sure what data needs to be returned to make a graph out of,
---SELECT statement can be modified
+--Returns all samples for a state within the given timeframe
+--Inputs (user_provided_state_code, provided_lower_date_limit, provided_upper_date_limit)
 SELECT DateLocal, p_s.id, units
-FROM pollutant_sample AS p_s, in_state, taken_at AS t_a
-WHERE DateLocal >= provided_lower_date_limit
+FROM pollutant_sample AS p_s, taken_at AS t_a, in_state
+INNER JOIN
+	ON p_s.sample_id == t_a.sample_id
+INNER JOIN
+	ON t_a.site_num == in_state.site_num
+WHERE in_state.state_code == user_provided_state_code
+	AND DateLocal >= provided_lower_date_limit
 	AND DateLocal <= provided_upper_date_limit
-	AND p_s.sample_id == taken_at.sample_id
-	AND taken_at.site_num == in_state.site_num
-	AND in_state.state_code == user_provided_state_code
 
---Date limits not provided
+--Returns all samples for a state
+--Inputs (user_provided_state_code)
 SELECT p_s.DateLocal, p_s.id, p_s.units
-FROM pollutant_sample AS p_s, taken_at, in_state	
-WHERE p_s.sample_id == taken_at.sample_id
-	AND taken_at.site_num  == in_state.site_num
-	AND in_state.state_code == user_provided_state_code
-
---Neither query needs to select from survey_site, unless
---you want City to be available in the return
-
---also not sure how to put these queries in a format that they
---become, like, usable functions
+FROM pollutant_sample AS p_s, taken_at AS t_a, in_state	
+INNER JOIN
+	ON p_s.sample_id == t_a.sample_id
+INNER JOIN
+	ON t_a.site_num == in_state.site_num
+WHERE in_state.state_code == user_provided_state_code
