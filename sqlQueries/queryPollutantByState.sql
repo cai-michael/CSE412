b05290ad0ad6e60f07368288a470e2bdfd6f1f53
Jack Summers <jsummers1221@gmail.com>
2020-10-12 22:55:56 -0700

    Added Michael's requested changes for averaging the pollutants' means across a state

--- sqlQueries/queryPollutantByState.sql
+++ sqlQueries/queryPollutantByState.sql
@@ -31,3 +31,20 @@ INNER JOIN pollutant_type AS ptype
     ON ptype.id = it.type_id
 WHERE in_state.state_code = user_provided_state_code
 ORDER BY date_local
+
+--Returns the average of the means of the pollutants across all sites in a state, for each date.
+--The data is ordered by date_local and pollutant name which is useful for creating a linegraph
+--Inputs (user_provided_state_code)
+SELECT psample.date_local, psample.id, ptype.name, AVG(psample.mean)
+FROM pollutant_sample AS psample
+	INNER JOIN taken_at AS ta
+		ON psample.sample_id = ta.sample_id
+	INNER JOIN in_state
+		ON ta.site_num = in_state.site_num
+	INNER JOIN is_type AS it
+		ON psample.id = it.sample_id
+	INNER JOIN pollutant_type AS ptype
+		ON ptype.id = it.type_id
+WHERE in_state.state_code = user_provided_state_code
+GROUP BY date_local, ptype.name
+ORDER BY ptype.name, date_local;
