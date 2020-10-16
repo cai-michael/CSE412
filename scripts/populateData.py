fc95d39c26f260b0effe2cfbfc9730cbdefb6b1a
Brennan Kuhman <bkuhman@asu.edu>
2020-10-15 20:33:08 -0700

    fixup connection line, comment out for now.

--- scripts/populateData.py
+++ scripts/populateData.py
@@ -1,12 +1,13 @@
 import json
+#import psycopg2
 
 with open("../config.json") as json_config_file:
     config = json.load(json_config_file)['database']
 
-conn = psycopg2.connect("dbname=config['dbname'], user=config['username'], password=['password'], host=['hosturl'])
+#conn = psycopg2.connect("dbname=config['dbname'], user=config['username'], password=['password'], host=['hosturl']")
 
 import csv
 csvData = open('../sampleData.csv', newline='')
 csvReader = csv.reader(csvfile, delimiter=',', quotechar='|')
 for row in csvReader:
-    print(', '.join(row))
+        print(', '.join(row))
