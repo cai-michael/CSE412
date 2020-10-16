fd815155f3825710b62137d8d1b3c1f78be5abff
cai-michael <mrcai@asu.edu>
2020-10-16 16:10:09 -0700

    Updated Query Set

--- sqlQueries/updatePollutantSample.sql
+++
@@ -1,8 +0,0 @@
-UPDATE pollutant_sample
-SET date_local = user_given_date_local,
-	max_hour = user_given_max_hour,
-	max_value = user_given_max_value,
-	aqi = user_given_aqi,
-	units = user_given_units,
-	mean = user_given_mean
-WHERE id = user_given_id;
