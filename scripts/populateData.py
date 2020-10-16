20ce20b5af50be4389ac957259d3299c7f741836
mrcai <mrcai@asu.edu>
2020-10-16 00:06:17 -0700

    Fixing Minor Errors

--- scripts/populateData.py
+++ scripts/populateData.py
@@ -4,10 +4,10 @@ import psycopg2
 import populateDataHelpers as helpers
 
 # Path to the CSV we want to populate from
-dataPath = '../sampleData.csv'
+dataPath = 'sampleData.csv'
 
 # Open Config File for Database Credentials
-with open("../config.json") as json_config_file:
+with open("config.json") as json_config_file:
     config = json.load(json_config_file)['database']
 
 # Make Databse Connection
@@ -16,22 +16,13 @@ cursor = conn.cursor()
 
 # Create Dictionaries to ensure consistency
 pTypes = { 'NO2':1, 'O3':2, 'SO2':3, 'CO':4 }
-helpers.populateTypes(cursor, pTypes)
 sites = { }
 counties = { }
 states = { }
 
+helpers.populateTypes(cursor, pTypes)
+
 # Open the CSV and start populating
-# Tables for reference:
-#pollutant_type
-#pollutant_sample
-#is_type
-#survey_site
-#taken_at
-#county
-#in_county
-#state
-#in_state
 counter = 0
 csvData = open(dataPath, newline='')
 csvReader = csv.DictReader(csvData, delimiter=',', quotechar='|')
@@ -55,7 +46,7 @@ for row in csvReader:
         sites[siteNum] = row['Address']
     
     # Grab Date
-    dateLocal = row['Date']
+    dateLocal = row['Date Local']
     
     # Populate Samples
     for pName, pNum in pTypes.items():
@@ -67,3 +58,8 @@ for row in csvReader:
         maxHour = row[pName + ' 1st Max Hour']
         aqi = row[pName + ' AQI']
         helpers.addSample(cursor, uniqueId, maxHour, maxValue, aqi, units, mean, siteNum, pNum, dateLocal)
+        conn.commit()
+
+# Close Connection
+cursor.close()
+conn.close()
