c2f315f0de297fff47f685a0cf003280de8d561a
cai-michael <mrcai@asu.edu>
2020-10-16 13:43:59 -0700

    Changed Sampled Data

--- scripts/sampleOriginalData.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-"""
-This is meant to sample data for the demo since there are thousands of entries and the script
-to populate the data only inserts at about 100 entires per minute.
-"""
-
-a = pd.read_csv('pollution_us_2000_2016.csv', delimiter=',')
-print('wow')
