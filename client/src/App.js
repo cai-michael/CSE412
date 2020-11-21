ce873ae1f3468e22e61001c5f6c0e47dfd4a7c77
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2020-11-21 02:59:01 -0700

    feat(client): select state and pollutant type

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {Fragment, useState, useEffect} from 'react'
 import BarChart from './BarChart'
 import './App.css'
 
@@ -13,18 +13,112 @@ const getData = (endpoint, key, body) => {
   }).then((r) => r.json())
 }
 
+const StateSelector = ({states, setStates}) => {
+  const selectState = (stateName) => (_) =>
+    setStates(states.map((state) => ({...state, checked: state.name === stateName})))
+
+  return (
+    <form>
+      {states.map((state) => (
+        <Fragment key={state.name}>
+          <label>
+            <input
+              name={state.name}
+              type="radio"
+              checked={state.checked}
+              onChange={selectState(state.name)}
+            />
+            {state.name}
+          </label>
+          <br />
+        </Fragment>
+      ))}
+    </form>
+  )
+}
+
+const PollutantSelector = ({pollutants, setPollutants}) => {
+  const selectPollutant = (pollutantName) => (_) =>
+    setPollutants(
+      pollutants.map((p) => {
+        if (pollutantName === p.name) return {...p, checked: !p.checked}
+        else return p
+      }),
+    )
+
+  return (
+    <form>
+      {pollutants.map((pollutant) => (
+        <Fragment key={pollutant.name}>
+          <label>
+            <input
+              name={pollutant.name}
+              type="checkbox"
+              checked={pollutant.checked}
+              onChange={selectPollutant(pollutant.name)}
+            />
+            {pollutant.name}
+          </label>
+          <br />
+        </Fragment>
+      ))}
+    </form>
+  )
+}
+
 export default () => {
+  const [states, setStates] = useState([
+    {id: 0, name: 'Arizona', checked: true},
+    {id: 1, name: 'California', checked: false},
+  ])
+  const [pollutants, setPollutants] = useState([
+    {name: 'O3', checked: true},
+    {name: 'CO', checked: false},
+    {name: 'SO2', checked: false},
+    {name: 'NO2', checked: false},
+  ])
+
   const [data, setData] = useState([])
+
+  const ENDPOINT = process.env.REACT_APP_ENDPOINT
+  const API_KEY = process.env.REACT_APP_API_KEY
 
   useEffect(() => {
     const effect = async () => {
-      const ENDPOINT = process.env.REACT_APP_ENDPOINT
-      const API_KEY = process.env.REACT_APP_API_KEY
-      const body = {queryType: 'pollutantByState', parameters: {state: 'Arizona'}}
-      setData(await getData(ENDPOINT, API_KEY, body))
+      const selectedState = states.filter((state) => state.checked)[0]
+      const selectedPollutants = pollutants.filter((p) => p.checked).map((p) => p.name)
+
+      if (selectedState && selectedPollutants.length !== 0) {
+        const body = {
+          queryType: 'pollutantByState',
+          parameters: {state: selectedState.name},
+        }
+        const results = await getData(ENDPOINT, API_KEY, body)
+        setData(
+          results
+            .filter((d) => selectedPollutants.includes(d[1]))
+            .map((d) => [new Date(d[0]), d[1], d[2]]),
+        )
+      }
+
+      // TODO: investigate alternate query types
+      // const body = {queryType: 'pollutantBySite', parameters: {city: 'San Jose'}}
+      // const body = {queryType: 'testRestAPI', parameters: {pollutant: 'NO2'}}
+      // const body = {
+      //   queryType: 'siteMeansForSpecifiedPollutant',
+      //   parameters: {pollutant: 'NO2'},
+      // }
     }
     effect()
-  }, [])
+  }, [states, pollutants, ENDPOINT, API_KEY])
 
-  return <BarChart data={data} />
+  return (
+    <>
+      <StateSelector {...{states, setStates}} />
+      <br />
+      <PollutantSelector {...{pollutants, setPollutants}} />
+      <br />
+      <BarChart {...{data}} />
+    </>
+  )
 }
--- client/src/BarChart.js
+++ client/src/BarChart.js
@@ -1,12 +1,21 @@
 import React, {useRef, useEffect} from 'react'
-import {select, scaleLinear, max, axisBottom, axisLeft} from 'd3'
+import {
+  select,
+  scaleLinear,
+  scaleTime,
+  min,
+  max,
+  axisBottom,
+  axisLeft,
+  timeWeek,
+} from 'd3'
 import './BarChart.css'
 
 const getColor = (pollutant) => {
-  if (pollutant === 'CO') return 'yellow'
-  else if (pollutant === 'NO2') return 'red'
-  else if (pollutant === 'O3') return 'blue'
-  else if (pollutant === 'SO2') return 'orange'
+  if (pollutant === 'O3') return 'orange'
+  else if (pollutant === 'CO') return 'blue'
+  else if (pollutant === 'SO2') return 'red'
+  else if (pollutant === 'NO2') return 'brown'
   else return 'black'
 }
 
@@ -14,44 +23,63 @@ export default ({data}) => {
   const container = useRef(null)
 
   useEffect(() => {
-    if (data.length === 0) return
-
     const width = 1335
-    const height = 975
+    const height = 800
     const padding = 30
 
     const svg = select(container.current)
     svg.attr('width', width).attr('height', height)
 
-    const xScale = scaleLinear()
-      .domain([0, data.length])
+    const dates = data.map((d) => d[0])
+    const xScale = scaleTime()
+      .domain([min(dates), max(dates)])
       .range([padding, width - padding])
 
     const yScale = scaleLinear()
       .domain([0, max(data.map((d) => d[2]))])
       .range([height - padding, padding])
 
-    const circles = svg.selectAll('circle').data(data).enter().append('circle')
+    const circles = svg.selectAll('circle').data(data)
+
     circles
       .attr('fill', (d) => getColor(d[1]))
-      .attr('cx', (d, i) => xScale(i))
+      .transition()
+      .attr('cx', (d) => xScale(d[0]))
       .attr('cy', (d) => yScale(d[2]))
       .attr('r', 3)
 
-    const xAxis = axisBottom().scale(xScale)
+    circles
+      .enter()
+      .append('circle')
+      .attr('fill', (d) => getColor(d[1]))
+      .transition()
+      .attr('cx', (d) => xScale(d[0]))
+      .attr('cy', (d) => yScale(d[2]))
+      .attr('r', 3)
+
+    circles.exit().transition().duration(5).remove()
+
+    const xAxis = axisBottom().scale(xScale).ticks(timeWeek.every(1))
     svg
-      .append('g')
+      .select('g#axisBottom')
+      .transition()
       .attr('class', 'axis')
       .attr('transform', `translate(0, ${height - padding})`)
       .call(xAxis)
 
     const yAxis = axisLeft().scale(yScale)
     svg
-      .append('g')
+      .select('g#axisLeft')
+      .transition()
       .attr('class', 'axis')
       .attr('transform', `translate(${padding}, 0)`)
       .call(yAxis)
   }, [data])
 
-  return <svg ref={container}></svg>
+  return (
+    <svg ref={container}>
+      <g id="axisBottom"></g>
+      <g id="axisLeft"></g>
+    </svg>
+  )
 }
