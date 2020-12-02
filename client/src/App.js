d3361e8d8880b9de04f1e440f7a709986cdbd13c
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2020-12-01 22:52:22 -0700

    chore(client): select by sites and counties

--- client/src/App.js
+++ client/src/App.js
@@ -13,55 +13,47 @@ const getData = (endpoint, key, body) => {
   }).then((r) => r.json())
 }
 
-const StateSelector = ({states, setStates}) => {
-  const selectState = (stateName) => (_) =>
-    setStates(states.map((state) => ({...state, checked: state.name === stateName})))
+const listSites = (endpoint, key) =>
+  getData(endpoint, key, {
+    queryType: 'findAllSiteNames',
+    parameters: {},
+  })
+
+const listCounties = (endpoint, key) =>
+  getData(endpoint, key, {
+    queryType: 'findAllCountyNames',
+    parameters: {},
+  })
+
+const listStates = (endpoint, key) =>
+  getData(endpoint, key, {
+    queryType: 'findAllStateNames',
+    parameters: {},
+  })
+
+const listTypes = (endpoint, key) => Promise.resolve([['O3'], ['CO'], ['SO2'], ['NO2']])
+
+const Selector = ({items, setItems}) => {
+  const selectItem = (itemName) => (_) =>
+    setItems(items.map((item) => ({...item, checked: item.name === itemName})))
 
   return (
     <form>
-      {states.map((state) => (
-        <Fragment key={state.name}>
-          <label>
-            <input
-              name={state.name}
-              type="radio"
-              checked={state.checked}
-              onChange={selectState(state.name)}
-            />
-            {state.name}
-          </label>
-          <br />
-        </Fragment>
-      ))}
-    </form>
-  )
-}
-
-const PollutantSelector = ({pollutants, setPollutants}) => {
-  const selectPollutant = (pollutantName) => (_) =>
-    setPollutants(
-      pollutants.map((p) => {
-        if (pollutantName === p.name) return {...p, checked: !p.checked}
-        else return p
-      }),
-    )
-
-  return (
-    <form>
-      {pollutants.map((pollutant) => (
-        <Fragment key={pollutant.name}>
-          <label>
-            <input
-              name={pollutant.name}
-              type="checkbox"
-              checked={pollutant.checked}
-              onChange={selectPollutant(pollutant.name)}
-            />
-            {pollutant.name}
-          </label>
-          <br />
-        </Fragment>
-      ))}
+      {items &&
+        items.map((item) => (
+          <Fragment key={item.name}>
+            <label>
+              <input
+                name={item.name}
+                type="radio"
+                checked={item.checked}
+                onChange={selectItem(item.name)}
+              />
+              {item.name}
+            </label>
+            <br />
+          </Fragment>
+        ))}
     </form>
   )
 }
@@ -74,59 +66,97 @@ const colorPollutant = (pollutant) => {
   else return 'black'
 }
 
-export default () => {
-  const [states, setStates] = useState([
-    {name: 'Arizona', checked: true},
-    {name: 'California', checked: false},
-  ])
-  const [pollutants, setPollutants] = useState([
-    {name: 'O3', checked: true},
-    {name: 'CO', checked: false},
-    {name: 'SO2', checked: false},
-    {name: 'NO2', checked: false},
-  ])
+const makeRequest = (queryType, city, county, state, pollutant) => {
+  if (queryType === 'pollutantBySiteAndType') {
+    return {queryType, parameters: {city, pollutant}}
+  } else if (queryType === 'pollutantByCountyAndType') {
+    return {queryType, parameters: {county, pollutant}}
+  } else if (queryType === 'pollutantByStateAndType') {
+    return {queryType, parameters: {state, pollutant}}
+  }
+}
 
+export default () => {
+  const [sites, setSites] = useState([])
+  const [counties, setCounties] = useState([])
+  const [states, setStates] = useState([])
+  const [types, setTypes] = useState([])
+  const [queries, setQueries] = useState([
+    {name: 'site and type', queryType: 'pollutantBySiteAndType', checked: true},
+    {name: 'county and type', queryType: 'pollutantByCountyAndType', checked: false},
+    {name: 'state and type', queryType: 'pollutantByStateAndType', checked: false},
+  ])
   const [data, setData] = useState([])
 
   const ENDPOINT = process.env.REACT_APP_ENDPOINT
   const API_KEY = process.env.REACT_APP_API_KEY
 
+  const addCheckboxes = (l) => l.map((r, i) => ({name: r[0], checked: i === 0}))
+
   useEffect(() => {
     const effect = async () => {
-      const selectedState = states.filter((state) => state.checked)[0]
-      const selectedPollutants = pollutants.filter((p) => p.checked).map((p) => p.name)
-
-      if (selectedState && selectedPollutants.length !== 0) {
-        const body = {
-          queryType: 'pollutantByState',
-          parameters: {state: selectedState.name},
-        }
-        const results = await getData(ENDPOINT, API_KEY, body)
-        setData(
-          results
-            .filter((d) => selectedPollutants.includes(d[1]))
-            .map((d) => [new Date(d[0]), d[1], d[2], colorPollutant(d[1])]),
-        )
-      }
-
-      // TODO: investigate alternate query types
-      // const body = {queryType: 'pollutantBySite', parameters: {city: 'San Jose'}}
-      // const body = {queryType: 'testRestAPI', parameters: {pollutant: 'NO2'}}
-      // const body = {
-      //   queryType: 'siteMeansForSpecifiedPollutant',
-      //   parameters: {pollutant: 'NO2'},
-      // }
+      setSites(addCheckboxes(await listSites(ENDPOINT, API_KEY)))
+      setCounties(addCheckboxes(await listCounties(ENDPOINT, API_KEY)))
+      setStates(addCheckboxes(await listStates(ENDPOINT, API_KEY)))
+      setTypes(addCheckboxes(await listTypes(ENDPOINT, API_KEY)))
     }
     effect()
-  }, [states, pollutants, ENDPOINT, API_KEY])
+  }, [ENDPOINT, API_KEY])
+
+  const selected = (items) => items?.filter((i) => i?.checked)[0]
+
+  useEffect(() => {
+    const effect = async () => {
+      const body = makeRequest(
+        selected(queries)?.queryType,
+        selected(sites)?.name,
+        selected(counties)?.name,
+        selected(states)?.name,
+        selected(types)?.name,
+      )
+
+      const isReady =
+        0 ===
+        Object.values(body.parameters).filter((i) => i === null || i === undefined)
+          .length
+
+      if (isReady) {
+        const results = await getData(ENDPOINT, API_KEY, body)
+        const normalized = results.map((d) => {
+          if (d.length === 3) return [new Date(d[0]), d[1], d[2], colorPollutant(d[1])]
+          else return [new Date(d[0]), d[2], d[3], colorPollutant(d[2])]
+        })
+        setData(normalized)
+      }
+    }
+    effect()
+  }, [queries, sites, counties, states, types, ENDPOINT, API_KEY])
 
   return (
     <>
-      <StateSelector {...{states, setStates}} />
+      <Selector items={queries} setItems={setQueries} />
       <br />
-      <PollutantSelector {...{pollutants, setPollutants}} />
+      {selected(queries).queryType === 'pollutantBySiteAndType' && (
+        <>
+          <Selector items={sites} setItems={setSites} />
+          <br />
+        </>
+      )}
+      {selected(queries).queryType === 'pollutantByCountyAndType' && (
+        <>
+          <Selector items={counties} setItems={setCounties} />
+          <br />
+        </>
+      )}
+      {selected(queries).queryType === 'pollutantByStateAndType' && (
+        <>
+          <Selector items={states} setItems={setStates} />
+          <br />
+        </>
+      )}
+      <Selector items={types} setItems={setTypes} />
       <br />
-      <ScatterPlot {...{data}} />
+      <ScatterPlot data={data} />
     </>
   )
 }
